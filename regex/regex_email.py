b07305b22ce527eeea11f2a8c45c91d82cbbc550
Rimsha30laraib <141603431+Rimsha30laraib@users.noreply.github.com>
2024-02-12 22:22:59 +0500

    Add files via upload

---
+++ regex/regex_email.py
@@ -0,0 +1,15 @@
+import re
+
+
+
+pattern= r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+def match(text):
+    if(re.fullmatch(pattern,text)):
+        print("Valid Email")
+ 
+    else:
+        print("Invalid Email")
+
+text="rimshalaraib33@gmail"
+match(text)
